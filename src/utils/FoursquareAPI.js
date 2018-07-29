5f85f8878ee425f3911ab0bca5e68c9f10a1ae90
Ivan <ivan.silva_123@hotmail.com>
2018-07-29 22:36:20 +0100

    Alert Message for Errors

--- src/utils/FoursquareAPI.js
+++ src/utils/FoursquareAPI.js
@@ -4,8 +4,21 @@ const api = 'https://api.foursquare.com/v2/venues/';
 const version = '20180627';
 const lang = 'en';
 
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response.statusText);
+  }
+  return response;
+}
+
 export const getDetails = (id) =>
   fetch(`${api}${id}?client_id=${FS_CLIENT_ID}&client_secret=${FS_CLIENT_SECRET}&v=${version}&locale=${lang}`)
+    .then(handleErrors)
     .then(res => res.json())
     .then(data => data.response.venue)
-    .catch(err => alert('Couldn\'t retrieve venue details with ', err))
+    .catch(err => console.log('Couldn\'t retrieve venue details with ', err))
+    .then(function(response) {
+      alert("Unable to Load Places. Daily Quota Exceeded!");
+    }).catch(function(error) {
+        console.log(error);
+    });
